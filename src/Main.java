9b8f20fa186af27b39e1b2ce6297a7f1f4d7fe40
skrillex2007 <dropdopefam@gmail.com>
2024-12-13 17:57:03 +0300

    Доработка по замечании

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это сообщение создано в рамках разработки дополнения feature_1");
+        System.out.println("для Skillbox");
     }
 }
